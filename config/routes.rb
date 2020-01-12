0bee425582381e4843caa14a00fac031f1a366c9
Cameron Kolkey <Alleyria@gmail.com>
2020-01-12 12:45:07 +0100

    Shouts visible on dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     root to: "homes#show"
   end
 
+  resources :shouts, only: [:create]
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, only: [:create]
 
