79b910e90499aea3e6661741eb5bd062cf5f2e17
Cameron Kolkey <Alleyria@gmail.com>
2020-02-14 13:30:04 +0100

    Pulled Avatar helper into its own module changed how users were found in url

--- app/helpers/shout_helper.rb
+++ app/helpers/shout_helper.rb
@@ -1,18 +1,11 @@
 # frozen_string_literal: true
 
 module ShoutHelper
-  def avatar(user)
-    email_digest = Digest::MD5.hexdigest(user.email)
-    gravatar_url = "//gravatar.com/avatar/#{email_digest}"
-
-    image_tag gravatar_url
-  end
-
   def like_button(shout)
     if current_user.liked?(shout)
-      link_to "Unlike", unlike_shout_path(shout), method: :delete
+      link_to 'Unlike', unlike_shout_path(shout), method: :delete
     else
-      link_to "Like!", like_shout_path(shout), method: :post
+      link_to 'Like!', like_shout_path(shout), method: :post
     end
   end
 
