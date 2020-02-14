52e235c9a9920217e15f09a591c31f00f4846f6e
Cameron Kolkey <Alleyria@gmail.com>
2020-02-14 11:55:41 +0100

    Added likes and unlikes to shout

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,30 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   constraints Clearance::Constraints::SignedIn.new do
-    root to: "dashboards#show", as: :signed_in_root
+    root to: 'dashboards#show', as: :signed_in_root
   end
 
   constraints Clearance::Constraints::SignedOut.new do
-    root to: "homes#show"
+    root to: 'homes#show'
   end
 
-  resources :shouts, only: [:create, :show]
-  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resources :shouts, only: %i[create show] do
+    member do
+      post 'like' => 'likes#create'
+      delete 'unlike' => 'likes#destroy'
+    end
+  end
+  resources :passwords, controller: 'clearance/passwords', only: %i[create new]
   resource :session, only: [:create]
 
-  resources :users, only: [:create, :show] do
+  resources :users, only: %i[create show] do
     resource :password,
-      controller: "clearance/passwords",
-      only: [:edit, :update]
+             controller: 'clearance/passwords',
+             only: %i[edit update]
   end
 
-  get "/sign_in" => "sessions#new", as: "sign_in"
-  delete "/sign_out" => "sessions#destroy", as: "sign_out"
-  get "/sign_up" => "users#new", as: "sign_up"
+  get '/sign_in' => 'sessions#new', as: 'sign_in'
+  delete '/sign_out' => 'sessions#destroy', as: 'sign_out'
+  get '/sign_up' => 'users#new', as: 'sign_up'
 end
