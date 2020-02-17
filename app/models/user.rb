fccfcb9e0abd10c1c65ab23e87f876cbe1708c7c
Cameron Kolkey <Alleyria@gmail.com>
2020-02-17 11:33:55 +0100

    Add counter cache for users following

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
   has_many :likes
   has_many :liked_shouts, through: :likes, source: :shout
 
-  has_many :following_relationships, foreign_key: :follower_id
-  has_many :followed_users, through: :following_relationships
+  has_many :following_relationships, foreign_key: :follower_id, counter_cache: :followers_count
+  has_many :followed_users, through: :following_relationships, counter_cache: :followed_users_count
 
   def like(shout)
     liked_shouts << shout
