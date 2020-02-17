8188fcd51ff3e3f4b121468519340db662ef058b
Cameron Kolkey <Alleyria@gmail.com>
2020-02-17 16:38:08 +0100

    Hashtag Searching

---
+++ app/services/search.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class Search
+  attr_reader :term
+
+  def initialize(term:)
+    @term = term
+  end
+
+  def run
+    ShoutSearchQuery.new(term: "##{term}").to_relation
+  end
+  alias results run
+end
